cccf95b687acc44455a498ee8cbb7fd63a4632ad
AliVillalobos <ali.villalobos.dev@gamil.com>
2024-11-29 14:16:39 -0500

    last actualization

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -9,8 +9,6 @@ function ProductList() {
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
     const dispatch = useDispatch();
-   
-
     
     const plantsArray = [
         {
